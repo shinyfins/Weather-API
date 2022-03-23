a357d973eb5e913d6d7a1c58a67408653809628f
Your Name <mcpgco@gmail.com>
2022-03-22 19:52:40 -0700

    got console log to register the event

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city-pick");
+
+var formSubmitHandler = function(event){
+  event.preventDefault();
+  console.log(event);
+};
+
 var getUserRepos = function() {
     var response = fetch("https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=1894988d667eb06dee04a4291eb5803d").then(function(response){
 response.json().then(function(data){      
@@ -6,6 +14,12 @@ response.json().then(function(data){
 });
 }
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
+//getUserRepos();
+//info that we want for assignment 
+// search by city
+//current and future weather conditions
+//saved searches
+//temp wind humidiity
 
-
+//API TAGS TO USE MAIN - NAME
